66229aecaac3f59840bad75b77545e4c2957ce83
Michael Schmidt <mschmidt271@gmail.com>
2017-07-14 19:40:01 -0600

    update unit tests

--- modules-local/beamdyn/src/BeamDyn.f90
+++ modules-local/beamdyn/src/BeamDyn.f90
@@ -3453,7 +3453,8 @@ SUBROUTINE BD_GA2(t,n,u,utimes,p,x,xd,z,OtherState,m,ErrStat,ErrMsg)
       CALL BD_DistrLoadCopy( p, u_interp, m )
 
          ! Incorporate boundary conditions
-      CALL BD_BoundaryGA2(x,p,u_interp,OtherState)
+      CALL BD_BoundaryGA2(x,p,u_interp,OtherState, ErrStat2, ErrMsg2)
+          call SetErrStat(ErrStat2,ErrMsg2,ErrStat,ErrMsg,RoutineName)
 
 
       CALL BD_InitAcc( u_interp, p, x, OtherState, m, ErrStat2, ErrMsg2)
--- modules-local/beamdyn/tests/test_BD_CheckRotMat.f90
+++ modules-local/beamdyn/tests/test_BD_CheckRotMat.f90
@@ -1,5 +1,5 @@
 @test
-subroutine test_BD_CrvExtractCrv()
+subroutine test_BD_CheckRotMat()
     use pFUnit_mod
     use BeamDyn_Subs
     implicit none
@@ -7,8 +7,8 @@ subroutine test_BD_CrvExtractCrv()
     integer,    parameter    :: n = 1e3
     real(BDKi), parameter    :: pi1 = 4.0_BDKi * atan(1.0_BDKi)
     
-    REAL(BDKi) :: inmat(3, 3)
-    REAL(BDKi) :: outvec(3), testvec(3)
+    REAL(BDKi) :: inmat(3, 3), outmat(3, 3)
+    ! REAL(BDKi) :: outvec(3), testvec(3)
     integer    :: i, j
     REAL(BDKi) :: normvec(3, n)
     REAL(BDKi) :: angle(n)
@@ -25,7 +25,7 @@ subroutine test_BD_CrvExtractCrv()
         normvec(:, i) = normvec(:, i)/norm2(normvec(:, i))
         angle(i) = -pi1 + (real(i, BDKi)/real(n, BDKi)) * 2.0_BDKi * pi1
     end do
-
+    
     do i = 1, n
         do j = 1, n
             ct = cos(angle(j))
@@ -35,9 +35,9 @@ subroutine test_BD_CrvExtractCrv()
                                 normvec(1, i) * normvec(2, i) * omct + normvec(3, i) * st, ct + normvec(2, i)**2 * omct,                              normvec(2, i) * normvec(3, i) * omct - normvec(1, i) * st, &
                                 normvec(1, i) * normvec(3, i) * omct - normvec(2, i) * st, normvec(2, i) * normvec(3, i) * omct + normvec(1, i) * st, ct + normvec(3, i)**2 * omct /), &
                                 shape(inmat), order=(/2, 1/) )
-            testvec = 4.0_BDKi * tan(angle(j)/4.0_BDKi) * normvec(:, i)
-            call BD_CrvExtractCrv(inmat, outvec, ErrStat, ErrMsg)
-            @assertEqual(outvec, testvec, 1e-14)
+            inmat = inmat + 1e-14
+            call BD_CheckRotMat(inmat, outmat, ErrStat, ErrMsg)
+            @assertEqual(inmat, outmat, 1e-13)
         end do
     end do
 
@@ -49,11 +49,14 @@ subroutine test_BD_CrvExtractCrv()
 
             call random_seed(size = n)
             allocate(seed(n))
+
             call system_clock(count = clock)
+
             seed = clock + 37 * (/ (i - 1, i = 1, n) /)
             call random_seed(put = seed)
+
             deallocate(seed)
 
-        end subroutine init_random_seed
+        end subroutine
 
-end subroutine test_BD_CrvExtractCrv
+end subroutine test_BD_CheckRotMat
