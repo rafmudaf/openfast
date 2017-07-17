ffba85466ff7054ad4dc5d359b529792db14e0cc
Michael Schmidt <mschmidt271@gmail.com>
2017-07-17 11:37:36 -0600

    update and add commenting for tests

--- modules-local/beamdyn/tests/test_BD_CheckRotMat.F90
+++ modules-local/beamdyn/tests/test_BD_CheckRotMat.F90
@@ -1,32 +1,34 @@
-!> This unit test is for the subroutine BD_CrvExtractCrv and tests it by generating 'n' random unit vectors
-!> and testing 'n' angles in the range \f$[-\pi, \pi]\f$ for each unit vector to determine whether the
-!> subroutine generates the appropriate WM parameter vector from an explicity constructed rotation matrix,
-!> as compared the explicity calculated WM vector.
-! mjs-- NOTE: could presumably change 'n' and 'tol' to be inputs to the subroutine in the comprehensive
-    ! unit testing framework
+!> This unit test is for the subroutine BD_CheckRotMat and tests it by generating 'n' random unit vectors
+!> and testing 'n' angles in the range \f$[-\pi, \pi]\f$ for each unit vector. 
+!> First, rotation matrices are generated from these axis-angle pairs, passed to the subroutine, and
+!> compared to the original with tolerance 'tol1' to ensure that valid rotation matrices are not corrected.
+!> Next, the rotation matrices are perturbed by the factor 'pert', and the subroutine is run to determine
+!> whether the corrected rotation matrix lies within tolerance 'tol2' of the original.
+! mjs-- NOTE: could presumably change 'n', 'pert', and 'tol1'/'tol2' to be inputs to the subroutine in
+! the comprehensive unit testing framework
+! mjs--NOTE: Currrently, it appears 'tol2' must be an order of magnitude larger than 'pert'. Not sure if
+    ! this is a concern, though, considering that the subroutine will not correct valid rotation matrices.
 @test
-subroutine test_BD_CrvExtractCrv()
+subroutine test_BD_CheckRotMat()
     use pFUnit_mod
     use BeamDyn_Subs
     implicit none
 
     ! n is the parameter determining the size of the test
     integer,    parameter    :: n = 1e3
-    real(BDKi), parameter    :: tol = 1e-14
+    real(BDKi), parameter    :: tol1 = 1e-16, tol2 = 1e-13, pert = tol2*1e-1
     ! mjs--FIXME: currently this subroutine is not seeing NWTC_Num, but if this is fixed, the built in
         ! value for pi could be used instead of this
     real(BDKi), parameter    :: pi1 = 4.0_BDKi * atan(1.0_BDKi)
     
-    REAL(BDKi) :: inmat(3, 3)
-    REAL(BDKi) :: outvec(3), testvec(3)
+    REAL(BDKi) :: inmat(3, 3), outmat(3, 3), tempmat(3, 3)
     integer    :: i, j
     REAL(BDKi) :: normvec(3, n)
     REAL(BDKi) :: angle(n)
     REAL(BDKi) :: ct, st, omct
 
-
-    INTEGER(IntKi)             :: ErrStat ! Temporary Error status
-    CHARACTER(ErrMsgLen)       :: ErrMsg  ! Temporary Error message
+    INTEGER(IntKi)             :: ErrStat
+    CHARACTER(ErrMsgLen)       :: ErrMsg
 
     call init_random_seed
     call random_number(normvec)
@@ -47,10 +49,13 @@ subroutine test_BD_CrvExtractCrv()
                                 normvec(1, i) * normvec(2, i) * omct + normvec(3, i) * st, ct + normvec(2, i)**2 * omct,                              normvec(2, i) * normvec(3, i) * omct - normvec(1, i) * st, &
                                 normvec(1, i) * normvec(3, i) * omct - normvec(2, i) * st, normvec(2, i) * normvec(3, i) * omct + normvec(1, i) * st, ct + normvec(3, i)**2 * omct /), &
                                 shape(inmat), order=(/2, 1/) )
-            ! formula for WM parameters from Bauchau, 'Flexible Multibody Dynamics'
-            testvec = 4.0_BDKi * tan(angle(j)/4.0_BDKi) * normvec(:, i)
-            call BD_CrvExtractCrv(inmat, outvec, ErrStat, ErrMsg)
-            @assertEqual(outvec, testvec, tol)
+            call random_number(tempmat)
+            call BD_CheckRotMat(inmat, outmat, ErrStat, ErrMsg)
+            @assertEqual(inmat, outmat, tol1)
+            call random_number(tempmat)
+            inmat = inmat + pert * tempmat
+            call BD_CheckRotMat(inmat, outmat, ErrStat, ErrMsg)
+            @assertEqual(inmat, outmat, tol2)
         end do
     end do
 
@@ -70,4 +75,4 @@ subroutine test_BD_CrvExtractCrv()
 
         end subroutine init_random_seed
 
-end subroutine test_BD_CrvExtractCrv
+end subroutine test_BD_CheckRotMat
