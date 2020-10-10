4a6b8728997ecc490013a12807b1d62120c8e39d
Jake Nunemaker <jake.d.nunemaker@gmail.com>
2020-10-10 09:02:37 -0600

    Add tests cases to GetWords for mismatch in expected/found number of words.

--- modules/nwtc-library/tests/test_NWTC_GetWords.F90
+++ modules/nwtc-library/tests/test_NWTC_GetWords.F90
@@ -113,4 +113,54 @@ contains
 
     end subroutine
 
+    @test
+    subroutine test_partial_parse()
+
+        character(64)           :: case
+        character(64)           :: Words   (2)
+
+        case = "word otherword wordwithnumber1 wordwithsymbol&"
+        CALL GetWords(case, Words, 2)
+
+        @assertEqual(size(Words), 2)
+        @assertEqual(Words(1), "word")
+        @assertEqual(Words(2), "otherword")
+
+    end subroutine
+
+    ! FAILING CASES
+
+    @test
+    subroutine test_input_words_greater_than_found()
+
+        ! TODO: SHOULDN'T THIS CASE FAIL?
+        character(64)           :: case
+        character(64)           :: Words   (6)
+
+        case = "word otherword wordwithnumber1 wordwithsymbol&"
+        CALL GetWords(case, Words, 6)
+
+        @assertEqual(size(Words), 6)
+        @assertEqual(Words(1), "word")
+        @assertEqual(Words(2), "otherword")
+        @assertEqual(Words(3), "wordwithnumber1")
+        @assertEqual(Words(4), "wordwithsymbol&")
+        @assertEqual(Words(5), "")
+        @assertEqual(Words(6), "")
+
+    end subroutine
+
+    @test
+    subroutine test_blank_input_line()
+
+        ! TODO: SHOULDN'T THIS CASE FAIL?
+        character(64)           :: case
+        character(64)           :: Words   (4)
+
+        case = ""
+        CALL GetWords(case, Words, 4)
+        @assertEqual(size(Words), 4)
+
+    end subroutine
+
 end module
