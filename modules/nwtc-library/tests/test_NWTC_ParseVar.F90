65e5e1975c4e6b74eddbb02c695c835f7b1c253e
Jake Nunemaker <jake.d.nunemaker@gmail.com>
2020-10-10 15:53:14 -0600

    Add tests for type interface.

--- modules/nwtc-library/tests/test_NWTC_ParseVar.F90
+++ modules/nwtc-library/tests/test_NWTC_ParseVar.F90
@@ -158,17 +158,17 @@ contains
 
     end subroutine
 
-    @test
-    subroutine test_ParseDbVar()
+    ! @test
+    ! subroutine test_ParseDbVar()
 
-        type(FileInfoType)              :: InFileInfo
-        INTEGER(IntKi)                  :: LineNo
-        INTEGER(IntKi)                  :: TmpErrStat
-        CHARACTER(ErrMsgLen)            :: TmpErrMsg
+    !     type(FileInfoType)              :: InFileInfo
+    !     INTEGER(IntKi)                  :: LineNo
+    !     INTEGER(IntKi)                  :: TmpErrStat
+    !     CHARACTER(ErrMsgLen)            :: TmpErrMsg
 
-        REAL(DbKi)                      :: res1, res2, res3, res4, res5
+    !     REAL(DbKi)                      :: res1, res2, res3, res4, res5
 
-        InFileInfo = getTestInputFloats()
+    !     InFileInfo = getTestInputFloats()
 
         ! PASSING CASES
         ! LineNo = 1
@@ -195,6 +195,107 @@ contains
         ! CALL ParseDbVar(InFileInfo, LineNo, "HWindSpeed", res5, TmpErrStat, TmpErrMsg)
         ! @assertEqual(0, TmpErrStat)
         ! @assertEqual(0.0, res5)
+
+    ! end subroutine
+
+    @test
+    subroutine test_interface()
+
+        type(FileInfoType)              :: InLogicals
+        type(FileInfoType)              :: InIntegers
+        type(FileInfoType)              :: InReals
+        type(FileInfoType)              :: InCharacters
+
+        INTEGER(IntKi)                  :: LineNo
+        INTEGER(IntKi)                  :: TmpErrStat
+        CHARACTER(ErrMsgLen)            :: TmpErrMsg
+
+        LOGICAL                         :: logical_var
+        INTEGER(IntKi)                  :: integer_var
+        REAL(ReKi)                      :: real_var
+        CHARACTER(64)                   :: char_var
+
+
+        InLogicals = getTestInputLogicals()
+        InIntegers = getTestInputIntegers()
+        InReals = getTestInputFloats()
+        InCharacters = getTestInputCharacters()
+
+        ! PASSING CASES
+        ! These tests have compatible output variable types
+        LineNo = 1
+        CALL ParseVar(InLogicals, LineNo, "Echo", logical_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(0, TmpErrStat)
+        @assertEqual(.TRUE., logical_var)
+
+        LineNo = 1
+        CALL ParseVar(InLogicals, LineNo, "Echo", char_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(0, TmpErrStat)
+        @assertEqual("true", char_var)
+
+        LineNo = 1
+        CALL ParseVar(InIntegers, LineNo, "Windtype", integer_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(0, TmpErrStat)
+        @assertEqual(1, integer_var)
+
+        LineNo = 1
+        CALL ParseVar(InIntegers, LineNo, "Windtype", real_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(0, TmpErrStat)
+        @assertEqual(1.0, real_var)
+
+        LineNo = 1
+        CALL ParseVar(InIntegers, LineNo, "Windtype", char_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(0, TmpErrStat)
+        @assertEqual("1", char_var)
+
+        LineNo = 1
+        CALL ParseVar(InReals, LineNo, "HWindSpeed", real_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(0, TmpErrStat)
+        @assertEqual(8.0, real_var)
+
+        LineNo = 1
+        CALL ParseVar(InReals, LineNo, "HWindSpeed", char_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(0, TmpErrStat)
+        @assertEqual("8.0", char_var)
+
+        LineNo = 1
+        CALL ParseVar(InCharacters, LineNo, "UniformFileName", char_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(0, TmpErrStat)
+        @assertEqual("Wind/08ms.wnd", char_var)
+
+        ! FAILING CASES
+        ! These tests have incompatible output variable types
+        LineNo = 1
+        CALL ParseVar(InLogicals, LineNo, "Echo", real_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(4, TmpErrStat)
+
+        LineNo = 1
+        CALL ParseVar(InLogicals, LineNo, "Echo", integer_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(4, TmpErrStat)
+
+        LineNo = 1
+        CALL ParseVar(InCharacters, LineNo, "UniformFileName", real_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(4, TmpErrStat)
+
+        LineNo = 1
+        CALL ParseVar(InCharacters, LineNo, "UniformFileName", integer_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(4, TmpErrStat)
+
+        LineNo = 1
+        CALL ParseVar(InCharacters, LineNo, "UniformFileName", logical_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(4, TmpErrStat)
+
+        LineNo = 1
+        CALL ParseVar(InIntegers, LineNo, "Windtype", logical_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(4, TmpErrStat)
+
+        LineNo = 1
+        CALL ParseVar(InReals, LineNo, "HWindSpeed", logical_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(4, TmpErrStat)
+
+        LineNo = 1
+        CALL ParseVar(InReals, LineNo, "HWindSpeed", integer_var, TmpErrStat, TmpErrMsg)
+        @assertEqual(4, TmpErrStat)
 
     end subroutine
 
