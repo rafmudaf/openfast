ed0a37fce288b3ef2473a57f86118bd0fb121391
Jake Nunemaker <jake.d.nunemaker@gmail.com>
2020-10-10 08:44:24 -0600

    Add single seperator tests for GetWords.

---
+++ modules/nwtc-library/tests/test_NWTC_GetWords.F90
@@ -0,0 +1,98 @@
+module test_NWTC_GetWords
+
+    use pFUnit_mod
+    use NWTC_IO
+
+    implicit none
+
+
+contains
+
+    ! PASSING CASES
+
+    @test
+    subroutine test_whitespace_seperator()
+
+        character(64)           :: case
+        character(64)           :: Words   (4)
+
+        case = "word otherword wordwithnumber1 wordwithsymbol&"
+        CALL GetWords(case, Words, 4)
+
+        @assertEqual(size(Words), 4)
+        @assertEqual(Words(1), "word")
+        @assertEqual(Words(2), "otherword")
+        @assertEqual(Words(3), "wordwithnumber1")
+        @assertEqual(Words(4), "wordwithsymbol&")
+
+    end subroutine
+
+    @test
+    subroutine test_comma_seperator()
+
+        character(64)           :: case
+        character(64)           :: Words   (4)
+
+        case = "word,otherword,wordwithnumber1,wordwithsymbol&"
+        CALL GetWords(case, Words, 4)
+
+        @assertEqual(size(Words), 4)
+        @assertEqual(Words(1), "word")
+        @assertEqual(Words(2), "otherword")
+        @assertEqual(Words(3), "wordwithnumber1")
+        @assertEqual(Words(4), "wordwithsymbol&")
+
+    end subroutine
+
+    @test
+    subroutine test_semicolon_seperator()
+
+        character(64)           :: case
+        character(64)           :: Words   (4)
+
+        case = "word;otherword;wordwithnumber1;wordwithsymbol&"
+        CALL GetWords(case, Words, 4)
+
+        @assertEqual(size(Words), 4)
+        @assertEqual(Words(1), "word")
+        @assertEqual(Words(2), "otherword")
+        @assertEqual(Words(3), "wordwithnumber1")
+        @assertEqual(Words(4), "wordwithsymbol&")
+
+    end subroutine
+
+    @test
+    subroutine test_singlequote_seperator()
+
+        character(64)           :: case
+        character(64)           :: Words   (4)
+
+        case = "word'otherword'wordwithnumber1'wordwithsymbol&"
+        CALL GetWords(case, Words, 4)
+
+        @assertEqual(size(Words), 4)
+        @assertEqual(Words(1), "word")
+        @assertEqual(Words(2), "otherword")
+        @assertEqual(Words(3), "wordwithnumber1")
+        @assertEqual(Words(4), "wordwithsymbol&")
+
+    end subroutine
+
+    @test
+    subroutine test_doublequote_seperator()
+
+        character(64)           :: case
+        character(64)           :: Words   (4)
+
+        case = 'word"otherword"wordwithnumber1"wordwithsymbol&'
+        CALL GetWords(case, Words, 4)
+
+        @assertEqual(size(Words), 4)
+        @assertEqual(Words(1), "word")
+        @assertEqual(Words(2), "otherword")
+        @assertEqual(Words(3), "wordwithnumber1")
+        @assertEqual(Words(4), "wordwithsymbol&")
+
+    end subroutine
+
+end module
