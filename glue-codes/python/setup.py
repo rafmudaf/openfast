079de40524393276d8e1c62c2ec65c4c3f0f6044
Rafael M Mudafort <rafmudaf@gmail.com>
2021-09-29 11:49:06 -0500

    Add a Python project for the existing modules
    This allows the Python tools within OpenFAST to be pip-installed and solves the problem of path mangling.

---
+++ glue-codes/python/setup.py
@@ -0,0 +1,106 @@
+# Copyright 2021 NREL
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+# See https://floris.readthedocs.io for documentation
+
+
+# This setup file was taken from https://github.com/kennethreitz/setup.py
+# accessed on April 3, 2019.
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+from pathlib import Path
+
+from setuptools import Command, setup, find_packages
+
+
+# Package meta-data.
+NAME = "OpenFAST"
+DESCRIPTION = "A Python interface for the OpenFAST whole-turbine simulation software."
+URL = "https://github.com/OpenFAST/OpenFAST"
+EMAIL = "rafael.mudafort@nrel.gov"
+AUTHOR = "NREL National Wind Technology Center"
+REQUIRES_PYTHON = ">=3.7.0"
+
+# What packages are required for this module to be executed?
+REQUIRED = [    
+    "numpy>=1.16",
+]
+
+# What packages are optional?
+EXTRAS = {
+    # "docs": {},
+    # "develop": {},
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's VERSION module
+ROOT = Path(__file__).parent
+with open(ROOT / 'VERSION') as version_file:
+    VERSION = version_file.read().strip()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(
+        # where='openfast',
+        # include=['*'],
+        # exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    ),
+    # package_dir={"OpenFAST": "src"},
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license="Apache-2.0",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: OS Independent",
+    ],
+)
