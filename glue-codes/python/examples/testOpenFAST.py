e462cbfe7cab24e30f28d50dd2b909bc78461e2f
Henry <henry.korb@geo.uu.se>
2022-04-08 17:34:42 +0200

    make testOpenFAST more independent

--- glue-codes/python/examples/testOpenFAST.py
+++ glue-codes/python/examples/testOpenFAST.py
@@ -1,9 +1,10 @@
-
+#%%
 import OpenFAST as OF
 import numpy as np
-project_root = '/home/henry/Software/openFast/python_driver/openfast'
-library_path = project_root + '/install/lib/libopenfastlib.so'
-reg_test_path = project_root + "/reg_tests/r-test/glue-codes/openfast-cpp/5MW_Land_DLL_WTurb_cpp/5MW_Land_DLL_WTurb_cpp.fst"
+from pathlib import Path
+project_root = (Path(__file__).parent/ "../../../").resolve()
+library_path = str(project_root / 'install/lib/libopenfastlib.so')
+reg_test_path = str(project_root / "reg_tests/r-test/glue-codes/openfast-cpp/5MW_Land_DLL_WTurb_cpp/5MW_Land_DLL_WTurb_cpp.fst")
 # reg_test_path = project_root + "/reg_tests/r-test/glue-codes/openfast/AOC_YFix_WSt/AOC_YFix_WSt.fst"
 
 
@@ -22,22 +23,24 @@ def serial(input_file):
     # print(openfastlib.output_values[:,0])   # Prints the time steps
 
 def openfoam(input_file_name):
-    n_points_per_blade = 19
-    n_points_tower = 12
+    n_points_per_blade = 32
+    n_points_tower = 10
     turbine_position = (0,0,100)
     rotor_area = np.pi*63**2
     air_density = 1.225
 
     v_0 = 9
 
-    of = OF.OpenFastCoupled(input_file_name, 1, 0, 11., n_points_per_blade, n_points_tower, turbine_position, 0.5, rotor_area, air_density)
-    of.set_velocity(0, 0, 0,
-                     np.ones(n_points_per_blade*3)*v_0,  np.zeros(n_points_per_blade*3), np.zeros(n_points_per_blade*3), 
-                     np.ones(n_points_tower)*v_0, np.zeros(n_points_tower),np.zeros(n_points_tower))
-    of.fast_step()
+    of = OF.OpenFastCoupled(input_file_name, 1, 0, 25, n_points_per_blade, n_points_tower, turbine_position, 0.5, rotor_area, air_density)
+    for i in range(of.total_time_steps):
+        of.set_velocity(0, 0, 0,
+                        np.ones(n_points_per_blade*3)*v_0,  np.zeros(n_points_per_blade*3), np.zeros(n_points_per_blade*3), 
+                        np.ones(n_points_tower)*v_0, np.zeros(n_points_tower),np.zeros(n_points_tower))
+        of.fast_step()
     fx, fy, fz = of.get_blade_forces()
-    of.fast_end()
     print(fx)
+
+    of.fast_end()
 
 def parallel():
     ## Parallel with MPI
@@ -66,3 +69,5 @@ def parallel():
 
 if __name__=="__main__":
     openfoam(reg_test_path)
+
+# %%
