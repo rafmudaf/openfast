03672144ee7b71413ec0a37d7a2fc17c17c997a3
Rafael M Mudafort <rafmudaf@gmail.com>
2022-08-01 22:46:00 -0500

    Use external build script

---
+++ .github/scripts/oneapi/windows_compile_openfast.bat
@@ -0,0 +1,18 @@
+REM SPDX-FileCopyrightText: 2022 Intel Corporation
+REM
+REM SPDX-License-Identifier: MIT
+
+set VS_VER=%1
+
+@call "C:\Program Files (x86)\Intel\oneAPI\setvars-vcvarsall.bat" %VS_VER%
+
+for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
+@call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
+
+cd ${{runner.workspace}}\openfast\build
+cmake -G"NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH=${{runner.workspace}}\openfast\install -DCMAKE_BUILD_TYPE:STRING=Release ${GITHUB_WORKSPACE}
+@REM -DCMAKE_Fortran_COMPILER:STRING=${{env.FORTRAN_COMPILER}} \
+@REM  -DCMAKE_CXX_COMPILER:STRING=${{env.CXX_COMPILER}} \
+@REM  -DCMAKE_C_COMPILER:STRING=${{env.C_COMPILER}} \
+
+cmake --build . --target regression_test_module_drivers -- -j ${{env.NUM_PROCS}}
