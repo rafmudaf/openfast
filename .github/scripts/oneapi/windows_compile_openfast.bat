08e26d47a1554ab178847df2c26e8ca8da017780
Rafael M Mudafort <rafmudaf@gmail.com>
2022-08-01 22:57:41 -0500

    Use explicit paths in cmake command

--- .github/scripts/oneapi/windows_compile_openfast.bat
+++ .github/scripts/oneapi/windows_compile_openfast.bat
@@ -9,13 +9,15 @@ set VS_VER=%1
 for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\compiler\" ^| findstr /V latest ^| sort`) do @set "LATEST_VERSION=%%f"
 @call "C:\Program Files (x86)\Intel\oneAPI\compiler\%LATEST_VERSION%\env\vars.bat"
 
-cd ${{runner.workspace}}\openfast\build
-cmake .. ^
-  -G"NMake Makefiles" ^
-  -DCMAKE_INSTALL_PREFIX:PATH=${{runner.workspace}}\openfast\install ^
+@REM cd ${{runner.workspace}}\openfast\build
+cmake ^
+  -S "D:\a\openfast\openfast" ^
+  -B "D:\a\openfast\openfast\build" ^
+  -G "NMake Makefiles" ^
   -DCMAKE_BUILD_TYPE:STRING=Release
+@REM   -DCMAKE_INSTALL_PREFIX:PATH=${{runner.workspace}}\openfast\install ^
 @REM -DCMAKE_Fortran_COMPILER:STRING=${{env.FORTRAN_COMPILER}} \
 @REM  -DCMAKE_CXX_COMPILER:STRING=${{env.CXX_COMPILER}} \
 @REM  -DCMAKE_C_COMPILER:STRING=${{env.C_COMPILER}} \
 
-cmake --build . --target regression_test_module_drivers -- -j ${{env.NUM_PROCS}}
+cmake --build . --target beamdyn_driver -- -j ${{env.NUM_PROCS}}
