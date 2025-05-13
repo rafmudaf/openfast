992933f512a8b923cb61c095d4f752131ce48d81
Rafael M Mudafort <rafmudaf@gmail.com>
2025-05-12 21:56:55 -0500

    Revert "Partiall revert "Add MHK flag to ADI C interface""
    This reverts commit 895ec6f3a12d00b2b98b0fd5302c45b6147d8ff3.

--- glue-codes/labview/src/WaveTank.f90
+++ glue-codes/labview/src/WaveTank.f90
@@ -93,6 +93,7 @@ IMPLICIT NONE
     INTEGER(C_INT) :: NumTurbines_C
     INTEGER(C_INT) :: TransposeDCM
     INTEGER(C_INT) :: PointLoadOutput
+    INTEGER(C_INT) :: MHK
     INTEGER(C_INT) :: DebugLevel
     ! SetupRotor
     integer(c_int) :: iWT_c     !< Wind turbine / rotor number
@@ -221,6 +222,7 @@ IMPLICIT NONE
     NumTurbines_C = 1
     TransposeDCM = 1
     PointLoadOutput = 1      ! TODO: Use point load or distributed load?; 0 - distributed load, 1 - point load
+    MHK = MHK_Floating
     DebugLevel = 1
 
     ! ADI SetupRotor
@@ -272,6 +274,7 @@ IMPLICIT NONE
         NumTurbines_C,              &
         TransposeDCM,               &
         PointLoadOutput,            &
+        MHK,                        &
         DebugLevel,                 &
         ErrStat_C2,                 &
         ErrMsg_C2                   &
